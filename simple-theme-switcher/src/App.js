38d3890a2c31d5ef7a1c9a890dd5b836dffedab9
SergioSusanu <Sergio.susanu.developer@gmail.com>
2023-09-08 12:57:03 +0300

    simple theme switcher started

---
+++ simple-theme-switcher/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="checkbox" name="themeToggler" id="themeToggler" />
+    </div>
+  );
+}
+
+export default App;
