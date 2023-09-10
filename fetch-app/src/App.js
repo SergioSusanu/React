76b728b7c5711b2bd703ad548df1ab85dd0bf566
SergioSusanu <Sergio.susanu.developer@gmail.com>
2023-09-10 16:12:21 +0300

    simple fetch example ready

---
+++ fetch-app/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [btcData, setBtcData] = useState({})
+
+  const fetchBtc = ()=>{
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => response.json())
+      .then((jsonData) => setBtcData(jsonData.bpi.USD))
+      .catch((error) => console.log(error)); 
+
+  }
+
+  useEffect(()=>{
+    fetchBtc()
+  
+  },[])
+
+  return (
+    <div>
+      Bitcoin data
+      <h1>Current BTC/USD data</h1>
+      <p>Code: {btcData.code}</p>
+      <p>Symbol: {btcData.symbol}</p>
+      <p>Rate: {btcData.rate}</p>
+      <p>Description: {btcData.description}</p>
+      <p>Rate Float: {btcData.rate_float}</p>
+    </div>
+  );
+}
+
+export default App;
