d2124df07d238ca98f951947e38e728b7a067cbe
SergioSusanu <Sergio.susanu.developer@gmail.com>
2023-09-29 12:22:14 +0300

    styled the cards

--- simple-portfolio-landing/src/components/Card.js
+++ simple-portfolio-landing/src/components/Card.js
@@ -8,13 +8,20 @@ const Card = ({ title, description, imageSrc }) => {
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
   return (
-    <VStack>
-      <Image src={imageSrc} alt={title} />
-      <Heading as="h2" alignSelf="start">{title}</Heading>
-      <Text>{description}</Text>
-      <Link alignSelf="start"><span>See more </span>
-      <FontAwesomeIcon icon={faArrowRight} size="1x" /> 
-      </Link>
+    <VStack bg="white" borderRadius="10px">
+      <Image src={imageSrc} alt={title} borderRadius="10px" />
+      <VStack px="12px" pb="15px">
+        <Heading as="h2" color="black" fontSize="18px" alignSelf="start">
+          {title}
+        </Heading>
+        <Text fontSize="16px" color="GrayText">
+          {description}
+        </Text>
+        <Link alignSelf="start" color="black">
+          <span>See more </span>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </Link>
+      </VStack>
     </VStack>
   );
 };
